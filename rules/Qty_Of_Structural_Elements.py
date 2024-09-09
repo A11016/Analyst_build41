51f6af89a8ae30bc8f341605028ec0c49b62a503
A11016 <s214307@dtu.dk>
2024-09-09 15:34:54 +0200

    Create_Qty_Of_Structural_Elements.py

---
+++ rules/Qty_Of_Structural_Elements.py
@@ -0,0 +1,25 @@
+import ifcopenshell
+from bonsai.bim.ifc import IfcStore
+
+# Get the IFC file using IfcStore
+file = IfcStore.get_file()
+
+# List of IFC types and their corresponding full names
+ifc_elements = [
+    ('IfcColumn', 'Columns'),
+    ('IfcBeam', 'Beams'),
+    ('IfcWall', 'Walls'),
+    ('IfcRailing', 'Railings'),
+    ('IfcRamp', 'Ramps'),
+    ('IfcSlab', 'Slabs'),
+    ('IfcStair', 'Stairs'),
+    ('IfcCovering', 'Coverings'),
+    ('IfcStairFlight', 'Stair Flights')
+]
+
+# Loop through the list, get the elements by type, and print their quantities
+for ifc_type, full_name in ifc_elements:
+    elements = file.by_type(ifc_type)
+    print(f'Number of {full_name}: {len(elements)}')
+    
+    
