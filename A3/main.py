c786c3f151f0af6bde1d662119975c0087d78703
A11016 <s214307@dtu.dk>
2024-11-11 13:17:14 +0100

    Update main.py

--- A3/main.py
+++ A3/main.py
@@ -7,7 +7,7 @@ def cost_estimation_of_facade_elements(model, unit_cost=4196):
     total_surface_area = 0.0
     search_phrase = "Basic Wall:ARC - Ext. Wall"
     
-    # Iterate over all instances of IfcWall in the model
+    # Iterate over all instances of IfcCurtainWall in the model
     for wall in model.by_type("IfcCurtainWall"):
         # Check if "Basic Wall:Facade 400 mm" is part of the wall's name
         if wall.Name and search_phrase in wall.Name:
